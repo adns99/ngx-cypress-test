6f4cee15483667faccfe850103d98f65b9174c91
Adrian Sanabria <Adrian.Sanabria@udr.com>
2021-10-21 09:40:45 -0600

    Adding List & Dropdowns examples

--- cypress/integration/firstTestSpec.js
+++ cypress/integration/firstTestSpec.js
@@ -70,7 +70,7 @@ describe('First suite', () => {
 
     })
 
-    it.only('Then and Wrap Methods', () =>{
+    it('Then and Wrap Methods', () =>{
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -103,10 +103,119 @@ describe('First suite', () => {
                 expect(emailAddressLabelSecond).to.equal('Email address')
                 expect(passwordLabelSecond).to.equal('Password')
 
+                // Wrap Example
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
             })
         })
 
 
+    })
+
+    it('Invoke Commands', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // 1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+
+        // 2
+        cy.get('[for="exampleInputEmail1"]').then( label =>{
+            expect(label.text()).to.equal('Email address')
+        })
+
+
+        // 3 - Invoke Command
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+            expect(text).to.equal('Email address')
+        })
+
+
+        // 4 - Check box example
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            .should('contain', 'checked')
+
+
+         // 5 - Check box example 2
+         // Notice .click() was removed since the checkbox is already checked 
+         cy.contains('nb-card', 'Basic form')
+         .find('nb-checkbox')
+         .find('.custom-checkbox')
+         .invoke('attr', 'class')
+         .then( classValue =>{
+             expect(classValue).to.contain('checked') // Chai assertion
+         })
+
+    })
+
+    it('Assert property', () =>{
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        // Datepicker example
+        cy.contains('nb-card', 'Common Datepicker')
+        .find('input')
+        .then(input =>{
+            // As this is a Jquery element we cannot do .click so we need to wrap the element first
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Oct 17, 2021')
+        })
+
+    })
+
+    it('Radio buttons', () =>{
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons =>{
+            cy.wrap(radioButtons)
+            .first()
+            .check({force: true})
+            .should('be.checked')
+
+            cy.wrap(radioButtons)
+            .eq(1)
+            .check({force: true})
+
+            cy.wrap(radioButtons)
+            .first()
+            .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+            .eq(2)
+            .should('be.disabled')
+        })
+
+    })
+
+    it('Check boxes', () =>{
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        // cy.get('[type="checkbox"]').check({force: true}) 
+        cy.get('[type="checkbox"]').eq(0).click({force: true})
+
+    })
+
+    it.only('List and dropdowns', () =>{
+        cy.visit('/')
+
+        cy.get('nav nb-select').click()
+        
     })
 
 
